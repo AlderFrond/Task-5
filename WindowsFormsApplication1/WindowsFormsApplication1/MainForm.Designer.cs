bec8ec0ed2a9e980e0feebeec6d25c105f0abcef
AlderFrond <AlderFrond@yandex.ru>
2014-10-24 23:45:33 +0400

    Работа с формами

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnDiff = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Первое число: ";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(101, 12);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(110, 20);
+            this.txtFirstNumber.TabIndex = 0;
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(101, 64);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(23, 23);
+            this.btnSum.TabIndex = 2;
+            this.btnSum.Text = "+";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Второе число: ";
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(101, 38);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(110, 20);
+            this.txtSecondNumber.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Результат: ";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(101, 93);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(110, 20);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnDiff
+            // 
+            this.btnDiff.Location = new System.Drawing.Point(130, 64);
+            this.btnDiff.Name = "btnDiff";
+            this.btnDiff.Size = new System.Drawing.Size(23, 23);
+            this.btnDiff.TabIndex = 3;
+            this.btnDiff.Text = "-";
+            this.btnDiff.UseVisualStyleBackColor = true;
+            this.btnDiff.Click += new System.EventHandler(this.btnDiff_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(159, 64);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(23, 23);
+            this.btnMul.TabIndex = 4;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(188, 64);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(23, 23);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 126);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnDiff);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnDiff;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int sum = a + b;
+
+            txtResult.Text = sum.ToString();
+        }
+
+        private void btnDiff_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int diff = a - b;
+
+            txtResult.Text = diff.ToString();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int mul = a * b;
+
+            txtResult.Text = mul.ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int div = a / b;
+
+            txtResult.Text = div.ToString();
+        }
+
+        
+    }
+}
